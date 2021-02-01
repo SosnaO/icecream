5d38c9c58fdc3315b92da220b26e28df409af8d2
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-02-01 13:17:38 +0200

    Cleaning up the code. Fitting code to markup. Optimization of sizes for the desktop version. Removing unused files.

--- src/js/menu-scroll.js
+++
@@ -1,4 +0,0 @@
-window.onscroll = function() {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    document.getElementById('menu-wrapper').style.backgroundColor= scrolled==0?"transparent":"#E17992";
-  }
